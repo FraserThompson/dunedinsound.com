2c868a50df4b93b9365c6ce98065e6981dbb643c
Fraser Thompson <fraserthompson0@gmail.com>
2017-06-15 14:54:45 +1200

    Search by artist and venue name on gigs page

--- assets/js/isotope.js
+++ assets/js/isotope.js
@@ -5,11 +5,24 @@ isotopeConfig.isotopeInit = function() {
   isotopeConfig.qsRegex;
 
   isotopeConfig.filterFunction = function (elem) {
-    if (elem.querySelector('.name')) {
-      return isotopeConfig.qsRegex ? elem.querySelector('.name').innerHTML.match( isotopeConfig.qsRegex ) : true;
-    } else {
-      return true;
+    var string = "";
+    var name = elem.querySelector('.name');
+    var artistNames = elem.querySelector('.artistNames');
+    var venueName = elem.querySelector('.venueName');
+
+    if (name) {
+      string += name.innerHTML + ", ";
     }
+
+    if (artistNames) {
+      string += artistNames.innerHTML + ", ";
+    }
+
+    if (venueName) {
+      string += venueName.innerHTML;
+    }
+
+    return isotopeConfig.qsRegex && string ? string.match( isotopeConfig.qsRegex ) : true;
   }
 
   isotopeConfig.debounce = function( fn, threshold ) {
